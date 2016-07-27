95b0bb73e83fa40b1854d7767f797a562d09325f
Bute <butesa@ds.ad.adp.com>
2016-07-27 11:13:35 +0530

    hi

--- OurAnagram/src/Anagram.java
+++ OurAnagram/src/Anagram.java
@@ -4,11 +4,10 @@ public class Anagram {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-<<<<<<< HEAD
+
         System.out.println("Sayali");
-=======
         System.out.println("hello");
->>>>>>> 3a6a94c25e63556d1fd28c0681ed1f42d2ba1289
+        System.out.println("hi");
     }
 
 }
